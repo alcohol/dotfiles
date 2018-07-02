c71ae3bdeadb667cdeeee399d5c06cce1c363d82
Rob Bast <rob.bast@gmail.com>
2018-07-02 20:16:01 +0200

    order matters

--- .bashrc.d/osx.sh
+++ .bashrc.d/osx.sh
@@ -1,19 +1,5 @@
 #!/usr/bin/env bash
 
-# Expand PATH based on what is installed through homebrew, if installed.
-if command -v brew >/dev/null 2>&1; then
-  for dir in \
-    "/usr/local/opt/python/libexec/bin" \
-    "/usr/local/opt/coreutils/libexec/gnubin" \
-    "/usr/local/opt/findutils/libexec/gnubin" \
-    "/usr/local/opt/gnu-tar/libexec/gnubin" \
-    "/usr/local/opt/gnu-sed/libexec/gnubin" \
-    "/usr/local/opt/make/libexec/gnubin" \
-    "/usr/local/opt/sqlite/bin" \
-    "/usr/local/opt/curl/bin";
-    do [[ -d $dir ]] && PATH="$dir:$PATH"; done
-fi
-
 if (( ! EUID )); then
   return
 fi
