1901f156fbbd841092b165fd9be63229c5a48d3d
Rob Bast <rob.bast@gmail.com>
2020-06-02 12:30:44 +0200

    move/update some exports

--- .bashrc.d/exports.sh
+++ .bashrc.d/exports.sh
@@ -2,9 +2,9 @@
 
 # Make sure XDG_ variables are set regardless of environment,
 # and create their respective directories if they do not exist yet.
-export XDG_CONFIG_HOME=~/.config
-export XDG_CACHE_HOME=~/.cache
-export XDG_DATA_HOME=~/.local/share
+export XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-$HOME/.config}
+export XDG_CACHE_HOME=${XDG_CACHE_HOME:-$HOME/.cache}
+export XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
 
 for dir in "$XDG_CONFIG_HOME" "$XDG_CACHE_HOME" "$XDG_DATA_HOME"; do
   [[ ! -d $dir ]] && mkdir -p "$dir";
--- .bashrc.d/osx.sh
+++ .bashrc.d/osx.sh
@@ -4,6 +4,20 @@ if (( ! EUID )); then
   return
 fi
 
+# Expand PATH based on what is installed through homebrew, if installed.
+if command -v brew >/dev/null 2>&1; then
+  for dir in \
+    "/usr/local/opt/python/libexec/bin" \
+    "/usr/local/opt/coreutils/libexec/gnubin" \
+    "/usr/local/opt/findutils/libexec/gnubin" \
+    "/usr/local/opt/gnu-tar/libexec/gnubin" \
+    "/usr/local/opt/gnu-sed/libexec/gnubin" \
+    "/usr/local/opt/make/libexec/gnubin" \
+    "/usr/local/opt/sqlite/bin" \
+    "/usr/local/opt/curl/bin";
+    do [[ -d $dir ]] && PATH="$dir:$PATH"; done
+fi
+
 # Check if keychain is available, and if so, run it through eval to export
 # the environment variables SSH_AUTH_SOCK and SSH_AGENT_PID.
 if command -v keychain >/dev/null 2>&1; then
