1c108ad13bb3945b5a20235c6aa9d8787af0c602
Rob Bast <rob.bast@gmail.com>
2020-06-20 17:24:10 +0200

    make sure rtorrent session directory exists

--- .bashrc.d/_init.sh
+++ .bashrc.d/_init.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
 # clone Vundle and install Vim plugins
-if [[ ! -d $HOME/.vim/plugged ]]; then
+if [[ ! -d "$HOME/.vim/plugged" ]]; then
   curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
   vim +PlugInstall +qall &
+fi
+
+if [[ ! -d "$HOME/.cache/rtorrent" ]]; then
+  mkdir -p "$HOME/.cache/rtorrent/session"
 fi
