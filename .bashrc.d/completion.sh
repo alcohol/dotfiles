75e61705aaae0b146850ee3bb22814953234c25a
Rob Bast <rob.bast@gmail.com>
2021-04-28 15:57:32 +0200

    add google cloud sdk completion

--- .bashrc.d/completion.sh
+++ .bashrc.d/completion.sh
@@ -5,3 +5,10 @@ if [[ -f /usr/local/etc/bash_completion ]]; then
 elif [[ -f /usr/share/bash-completion/bash_completion ]]; then
   source /usr/share/bash-completion/bash_completion
 fi
+
+for file in \
+  "$HOME/.cache/yay/google-cloud-sdk/pkg/google-cloud-sdk/opt/google-cloud-sdk/completion.bash.inc" \
+  "$HOME/.cache/yay/google-cloud-sdk/pkg/google-cloud-sdk/opt/google-cloud-sdk/path.bash.inc";
+do
+  [[ -f "$file" ]] && source "$file"
+done
