4fff22610d5221fc32f7e38876c2301a02c8036f
Rob Bast <rob.bast@gmail.com>
2021-08-23 09:48:47 +0200

    add runtime dir env var

--- .bashrc.d/exports.sh
+++ .bashrc.d/exports.sh
@@ -14,6 +14,7 @@ export GIT_EDITOR=$EDITOR
 export XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-$HOME/.config}
 export XDG_CACHE_HOME=${XDG_CACHE_HOME:-$HOME/.cache}
 export XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
+export XDG_RUNTIME_DIR=/run/user/$(id -u)
 
 for dir in "$XDG_CONFIG_HOME" "$XDG_CACHE_HOME" "$XDG_DATA_HOME"; do
   [[ ! -d $dir ]] && mkdir -p "$dir";
